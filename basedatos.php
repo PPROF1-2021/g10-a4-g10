261cba90f6b573d489cb4332f1c2df05073db75d
lalorellano <estudiantes.ispc@gmail.com>
2021-11-17 02:03:41 -0300

    actualixacion de archivos php,  corregido ok muestra de datos, solo queda una linea de error/ falta formato

--- basedatos.php
+++ basedatos.php
@@ -1,8 +1,8 @@
 <?php
 $host:"localhost";
-$usuario:"id17838200_estudiantes";
-$clave:"Estudiantesispc+2021*";
-$basededatos:"id17838200_arbol";
+$usuario:"arbol123";
+$clave:"arbol+2021*";
+$basededatos:"podacba";
 
 $conn new mysqli ($host, $usuario, $clave, $basededatos); 
         mysqli_query ($conn, "SET character_set_result=utf8");
--- enviardatos.php
+++ enviardatos.php
@@ -1,39 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="shortcut icon" href="css/favicon.png" >
+    <title>Guardar Mensaje del Cliente</title>
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+<p> <a href="index.html"> Regresar</a> </p>
+<input type="button" value="Regresar"onclick="pVolver();"> <br>
+
+<input type='button' value='Submit' id='submit' onclick='paginaRedirect();'> <br/>
+ 
+ <!-- Message -->
+ <div id="mensaje"></div>
+ 
 <?php
-if (isset($_POST['submit'])) {
-    $from = "estudiantes.ispc@gmail.com"; 
-    $fromName = "Planta un árbol-WEB"; 
-    $to = " id17838200_estudiantes@2a02:4780:bad:c0de::13"; 
-    $subject = $_POST['Mensaje entregado'];
 
-    /* Componemos el mensaje */
-    $fullMessage = "Hola! " . $to . "\r\n";
-    $fullMessage .= $_POST['validarNombre'] . " " . $_POST['validarApellidos'] . " te ha enviado un mensaje:\r\n";
-    $fullMessage .= "\r\n";
-    $fullMessage .= "Nombre: " . $_POST['validarNombre'] . "\r\n";
-    $fullMessage .= "Apellidos: " . $_POST['validarApellidos'] . "\r\n";
-    $fullMessage .= "E-Mail: " . $_POST['validarEmail'] . "\r\n";
-    $fullMessage .= "Teléfono: " . $_POST['validarTelefono'] . "\r\n";
-    $fullMessage .= "Tema: " . $_POST['validarTema'] . "\r\n";
-    $fullMessage .= "Asunto: " . $_POST['validarAsunto'] . "\r\n";
-    $fullMessage .= "Mensaje: " . $_POST['validationMensaje'] . "\r\n";
-    $fullMessage .= "\r\n";
-    $fullMessage .= "Saludos!\r\n";
+$nombre = $_POST["validarNombre"];
+$apellido = $_POST["validarApellidos"];
+$direccion = $_POST["validarEmail"];
+$telefono = $_POST ["validarTelefono"];
 
-    /* Creamos las cabeceras del Email */
-    $headers = "Organization: RPF WEB\r\n";
-    $headers .= "From: " . $fromName . "<" . $from . ">\r\n";
-    $headers .= "Reply-To: " . $_POST['validarEmail'] . "\r\n";
-    $headers .= "Return-Path: " . $to . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
-    $headers .= "X-Priority: 3\r\n";
-    $headers .= "X-Mailer: PHP". phpversion() ."\r\n";
 
-    /* Enviamos el Formulario*/
-    if (mail($to, $subject, $fullMessage, $headers)) {
-        echo "<center><h2>El E-Mail se ha enviado correctamente!</h2></center>";
-    } else {
-        echo "<center><h2>Ops ! El envio ha fallado!</h2></center>S";
-    }
+print "  <p>Su nombre <strong>$nombre</strong>.</p>\n";
+print "\n";
+print "  <p>Su apellido <strong>$apellido</strong>.</p>\n";
+print "\n";
+print "  <p>Su mail <strong>$direccion</strong> .</p>\n";
+print "\n";
+print "  <p>Su telefono <strong>$telefono</strong>.</p>\n";
+print "\n";
+
+
+
+// Incluimos los datos de conexión y las funciones:
+include("basedatos.php");
+$con = mysqli_connect($host,$usuario,$clave,$basededatos) or die ("No se ha podido conectar al servidor de Base de datos");
+if (!$con) {
+    die("Conexión fallida: " . mysqli_connect_error());
 }
+
+
+
+$db = mysqli_select_db($con, $basededatos) or die ( "Upps! no se ha podido conectar a la base de datos" );
+$consulta = "INSERT INTO usuarios (nombre, apellido, correo, telefono) VALUES ('$nombre', '$apellido', '$direccion', '$telefono' )";
+
+// Usamos esas variables:
+if (mysqli_query ($con, $consulta)){
+    echo "<h3>Mensaje Guardado.</h3>";
+    } else {
+    echo "<h3>No fue posible guardar du mensaje</h3>";
+    echo "Error: " . $consulta . "<br>" . mysqli_error($con);
+ }
+ mysqli_close($con);
+
+ echo "<p style='font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 12px;color: #CC6600;'>".$row['estilo']."</p>";?>
 ?>
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+
+</body>
+</html>
